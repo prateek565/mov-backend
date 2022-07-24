32dacc4af64af63b3246df4a48da8a861ba1cd32
prateek565 <78357778+prateek565@users.noreply.github.com>
2022-07-24 14:24:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const app = express()
 
 app.use(express.json());
 app.use(cors()); 
-   
+
+app.use(express.static('public')); 
 const port = process.env.PORT || 5000;
 
 require('dotenv').config(); 

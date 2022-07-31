04be8f0c1e8a7b485657585134c00be8a732fa0f
prateek565 <78357778+prateek565@users.noreply.github.com>
2022-07-31 10:41:40 +0530

    Update database.js

--- model/database.js
+++ model/database.js
@@ -1,6 +1,6 @@
  const mongoose = require('mongoose');
  
- mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+ mongoose.connect("mongodb://prateek12345:prateek12345@ac-wgbllll-shard-00-00.zytdjxc.mongodb.net:27017,ac-wgbllll-shard-00-01.zytdjxc.mongodb.net:27017,ac-wgbllll-shard-00-02.zytdjxc.mongodb.net:27017/?ssl=true&replicaSet=atlas-n93c9i-shard-0&authSource=admin&retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true });
 
  const db = mongoose.connection;
  db.on('error', console.error.bind(console, 'connection error:'));
